c5b3f7954915da722583270c9e5d9bb256768fb6
subham141198 <banerjee.subham1411@gmail.com>
2023-05-04 13:21:00 +0530

    register button removed from navbar

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -39,8 +39,8 @@ function ChefNavbar() {
                   <NavDropdown.Divider />
                   <NavDropdown.Item onClick={handleLogout}>Sign Out</NavDropdown.Item>
                 </NavDropdown></>
-                  : <><Nav.Link href="/login">Login</Nav.Link>
-                    <Nav.Link href="/register">Register</Nav.Link></>
+                  : <Nav.Link href="/login">Login</Nav.Link>
+                    
               }
             </Nav>
           </Navbar.Collapse>
